288f91038f8215cfb2161c59efb4dc2ac92b0a18
Florian Hof <florian@speleo.ch>
2016-01-31 22:25:22 +0100

    author as username instead of userid in audit table on Liferay portal

--- src/ch/speleo/scis/persistence/audit/EntityTrackingRevisionListenerImpl.java
+++ src/ch/speleo/scis/persistence/audit/EntityTrackingRevisionListenerImpl.java
@@ -2,10 +2,12 @@ package ch.speleo.scis.persistence.audit;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.envers.EntityTrackingRevisionListener;
 import org.hibernate.envers.RevisionType;
+import org.openxava.util.UserInfo;
 import org.openxava.util.Users;
 
 import ch.speleo.scis.model.common.Revision;
@@ -13,14 +15,16 @@ import ch.speleo.scis.model.common.Revision;
 public class EntityTrackingRevisionListenerImpl 
 implements EntityTrackingRevisionListener {
 	
+	private static final Log loger = LogFactory.getLog(EntityTrackingRevisionListenerImpl.class);
+	
     public void newRevision(Object revisionEntity) {
     	
     	Revision revision = (Revision) revisionEntity;
-		String username = Users.getCurrent();
+		UserInfo userinfo = Users.getCurrentUserInfo();
+		String username = StringUtils.isNotBlank(userinfo.getNickName()) ? userinfo.getNickName() : userinfo.getId();
 		if (username != null) {
 			revision.setUsername(username);
 		} else {
-			Log loger = LogFactory.getLog(EntityTrackingRevisionListenerImpl.class);
 			loger.warn("cannot set a user id as worker of revision because nobody is logged in");
 		}
     }
