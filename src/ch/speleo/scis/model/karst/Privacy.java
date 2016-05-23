c86a519431fba37fcc4b88173e6dd251d6aafe45
Florian Hof <florian@speleo.ch>
2016-05-23 17:35:22 +0200

    more user-friendly editing of privacy

--- src/ch/speleo/scis/model/karst/Privacy.java
+++ src/ch/speleo/scis/model/karst/Privacy.java
@@ -54,7 +54,7 @@ extends GenericIdentity implements Serializable {
     /**
      * Date when the karst object started being protected.
      */
-    @Column(name = "START_DATE", nullable = false)
+    @Column(name = "START_DATE", nullable = true)
     @Temporal(TemporalType.DATE)
     //@Required
     private Date startDate;
@@ -69,7 +69,7 @@ extends GenericIdentity implements Serializable {
     /**
      * Why we decided to protect the given karst object.
      */
-    @Column(name = "REASON", nullable = false)    
+    @Column(name = "REASON", nullable = true)    
     //@Required @NotEmpty
 	@DisplaySize(value=100) 
     private String reason;
