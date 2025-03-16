638c2c0f99baf8bb473be0484c01e33de09e304d
florianhof <florian@speleo.ch>
2025-03-16 22:51:08 +0100

    extend podium with cave type, canton, and more
    - add cave type (natural or not, etc.)
    - add canton(s), can be several, comma-separated
    - add list of caves with most entrances
    - filter out the deleted caves and entrances

--- src/ch/speleo/scis/business/Podium.java
+++ src/ch/speleo/scis/business/Podium.java
@@ -2,7 +2,9 @@ package ch.speleo.scis.business;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.apache.commons.lang.text.StrBuilder;
@@ -52,7 +54,8 @@ public class Podium {
 		}
 		StrBuilder queryStr = new StrBuilder();
 		queryStr.append("from ").append(SpeleoObject.class.getSimpleName());
-		queryStr.append(" where ").append("length").append(" is not null ");
+		queryStr.append(" where ").append("deleted").append(" is not true ");
+		queryStr.append("   and ").append("length").append(" is not null ");
 		queryStr.append(" order by ").append("length").append(" desc ");
 		TypedQuery<SpeleoObject> query = XPersistence.getManager().createQuery(queryStr.toString(), SpeleoObject.class);
 		query.setMaxResults(nbCaves);
@@ -72,13 +75,39 @@ public class Podium {
 				"       else elevation end "; // in SQL super-fast, otherwise too slow
 		StrBuilder queryStr = new StrBuilder();
 		queryStr.append("from ").append(SpeleoObject.class.getSimpleName());
-		queryStr.append(" where ").append("depthAndElevation").append(" is not null ");
+		queryStr.append(" where ").append("deleted").append(" is not true ");
+		queryStr.append(" and(").append("depthAndElevation").append(" is not null ");
 		queryStr.append("    or ").append("depth").append(" is not null ");
 		queryStr.append("    or ").append("elevation").append(" is not null ");
+		queryStr.append("    )");
 		queryStr.append(" order by ").append(depthAndElevationComputedExpr).append(" desc ");
 		TypedQuery<SpeleoObject> query = XPersistence.getManager().createQuery(queryStr.toString(), SpeleoObject.class);
 		query.setMaxResults(nbCaves);
 		return query.getResultList();
+	}
+
+	public List<SpeleoObject> getCavesWithMostEntrances(Integer nbCaves) {
+		if (nbCaves == null) {
+			nbCaves = NB_RESULT_PER_DEFAULT;
+		} else if (nbCaves > NB_RESULT_MAX) {
+			nbCaves = NB_RESULT_MAX;
+		}
+		StrBuilder queryStr = new StrBuilder();
+		queryStr.append("select cave.id ");
+		queryStr.append("from SPELEO_OBJECT cave ");
+		queryStr.append("join KARST_OBJECT cave_karst_obj on cave.id = cave_karst_obj.id ");
+		queryStr.append("left join GROUND_OBJECT entrance on entrance.speleo_object_id = cave.id ");
+		queryStr.append("left join KARST_OBJECT entrance_karst_obj on entrance.id = entrance_karst_obj.id ");
+		queryStr.append(" where ").append("cave_karst_obj.deleted").append(" is not true ");
+		queryStr.append("   and ").append("entrance_karst_obj.deleted").append(" is not true ");
+		queryStr.append(" group by cave.id");
+		queryStr.append(" order by ").append("count(entrance.id)").append(" desc ");
+		Query query = XPersistence.getManager().createNativeQuery(queryStr.toString());
+		query.setMaxResults(nbCaves);
+		List<Number> caveIds = query.getResultList();
+		return caveIds.stream()
+			.map(caveId -> XPersistence.getManager().find(SpeleoObject.class, caveId.longValue()))
+			.collect(Collectors.toList());
 	}
 	
 }
